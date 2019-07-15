1a5131b7ded60a3464bbf1d4ee39e187c8c79b30
Bruna Neves <brubsnev@gmail.com>
2019-07-15 15:41:45 -0300

    cavy for end-to-end test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Routes from './routes';
 import './config/StatusBarConfig';
 import { Provider } from 'react-redux';
-
 import store from './store';
 
 import { Tester, TestHookStore } from 'cavy';
